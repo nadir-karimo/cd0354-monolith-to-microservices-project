7ca38a9d32580a5cb87cccf9d0218a12ca5fe612
Nadir Karimo <nadircamal@gmail.com>
2022-11-29 09:31:54 +0200

    fix configurations and host url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://acd679b42cc30422bae50f2a14649e25-248593750.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
